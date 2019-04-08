e1946216088af12709397ffa9b72dfaa62efea44
Guido Rombolá <guidorombola@gmail.com>
2019-04-08 20:05:38 -0300

    Prueba de conexion a multiples access points

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup(){
+  Serial.begin(115200);
 }
+ 
+void loop(){
+  Serial.println("Start scan");
+  int n = WiFi.scanNetworks();
+  for (int i = 0; i < n; ++i) {
+    if(WiFi.SSID(i) == "Moto"){
+      WiFi.begin("Moto","12345678");
+      if(WiFi.status() == WL_CONNECTED){
+        Serial.println("Conectado a "+ WiFi.SSID(i));
+      }
+    } else if(WiFi.SSID(i) == "AP2"){
+      WiFi.begin("AP2","password");
+      if(WiFi.status() == WL_CONNECTED){
+        Serial.println("Conectado a "+ WiFi.SSID(i));
+      }
+    }
+  }
+  delay(5000);
+  }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+  
