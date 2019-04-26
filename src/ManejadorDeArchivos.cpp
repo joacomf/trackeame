d4c37e2214111cde03568bc420031ade7f592cb9
Guido Rombolá <guidorombola@gmail.com>
2019-04-26 19:05:28 -0300

    Crea ManejadorDeArchivos

---
+++ src/ManejadorDeArchivos.cpp
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+#include "ManejadorDeArchivos.hpp"
+#include "SD.h"
+#include <SPI.h>
+
+// Define CS pin for the SD card module
+#define SD_CS 5
+
+ManejadorDeArchivos::ManejadorDeArchivos(){
+    bool tarjetaInicializada = SD.begin(SD_CS);  
+    if(!tarjetaInicializada) {
+        Serial.println("Card Mount Failed");
+        return;
+    }
+    uint8_t cardType = SD.cardType();
+    if(cardType == CARD_NONE) {
+        Serial.println("No SD card attached");
+        return;
+    }
+    //Crea el archivo de almacenamiento de posiciones (si no existe)
+    if(!SD.exists("/data.csv")){
+        File file = SD.open("/data.csv", FILE_WRITE);
+        file.close();
+    }
+    
+}
+
+void ManejadorDeArchivos::escribir(String buffer[], int longitud){
+    File file = SD.open("/data.csv", FILE_APPEND);
+    bool escrituraSatisfactioria = true;
+    if(!file) {
+        Serial.println("Fallo al abrir archivo para append");
+        return;
+    }
+    for (int i = 0; i < longitud; i++){
+        if(!file.println(buffer[i])) {
+            escrituraSatisfactioria = false;
+        }    
+    }
+    if(!escrituraSatisfactioria){
+        Serial.println("Hubo fallo en la escritura de alguna linea");
+    }
+    Serial.println("Escritura satisfactoria");
+    file.close();
+}
---
+++ src/ManejadorDeArchivos.hpp
@@ -0,0 +1,11 @@
+#ifndef MANEJADORDEARCHIVOS_H
+#define MANEJADORDEARCHIVOS_H
+
+class ManejadorDeArchivos{
+    public:
+        ManejadorDeArchivos();
+        void escribir(String buffer[], int longitudBuffer);
+        
+};
+
+#endif
