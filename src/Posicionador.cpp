95057072de14c9f947bdec374b953d856e2ebb67
Joaquín Moreno Fernández <joaquin.moreno@grupoesfera.com.ar>
2019-05-17 14:58:41 -0300

    Agrega posicionador para tomar muestras del GPS

---
+++ src/Posicionador.cpp
@@ -0,0 +1,22 @@
+#include "Posicionador.hpp"
+
+#define RXD2 16
+#define TXD2 17
+
+Posicionador::Posicionador(){
+    Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+}
+
+vector<string> Posicionador::obtenerPaqueteDePosiciones(){
+    vector <string> buffer;
+
+    Serial.println("Inicio carga");
+    while (Serial2.available() || buffer.size() < 10){
+        string localizacion = Serial2.readStringUntil('\n').c_str();
+        if (localizacion.substr(0,6) == "$GPRMC"){
+            buffer.push_back(localizacion);
+        }
+    }
+
+    return buffer;
+}
