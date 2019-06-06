e58e643d115fc4be9d220334c5fd002c462034e9
Guido Rombolá <guidorombola@gmail.com>
2019-06-06 20:26:10 -0300

    Agrega metodos para calcular distancia entre dos puntos

--- src/Posicionador.cpp
+++ src/Posicionador.cpp
@@ -3,6 +3,8 @@
 #define RXD2 16
 #define TXD2 17
 
+#define d2r (M_PI / 180.0)
+
 Posicionador::Posicionador(){
     Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
 }
@@ -20,4 +22,24 @@ vector<string> Posicionador::obtenerPaqueteDePosiciones(){
     }
 
     return buffer;
+}
+
+double Posicionador::distanciaEnMetrosEntre(double latitud_1, double longitud_1, double latitud_2, double longitud_2){
+    
+    double diferenciaLongitud = (longitud_2 - longitud_1) * d2r;
+    double diferenciaLatitud = (latitud_2 - latitud_1) * d2r;
+    double a = pow(sin(diferenciaLatitud/2.0), 2) + cos(latitud_1*d2r) * cos(latitud_2*d2r) * pow(sin(diferenciaLongitud/2.0), 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1-a));
+    double distanciaEnMetros = 6371000 * c;
+
+    return distanciaEnMetros;
+}
+
+double Posicionador::transformarPosicionAGrados(double posicion){
+    double longitudDecimal = (posicion / 100.0);
+    int grados = (int) longitudDecimal;
+
+    double resto = (longitudDecimal - grados) * 100.0 / 60;
+
+    return (grados + resto);
 }
--- src/Posicionador.hpp
+++ src/Posicionador.hpp
@@ -3,6 +3,7 @@
 
 #include <Arduino.h>
 #include <bits/stdc++.h> 
+#include <math.h>
 
 using namespace std;
 
@@ -10,6 +11,9 @@ class Posicionador{
     public:
         Posicionador();
         vector<string> obtenerPaqueteDePosiciones();
+    private:
+        double distanciaEnMetrosEntre(double latitud_1, double longitud_1, double latitud_2, double longitud_2);
+        double transformarPosicionAGrados(double posicion);
 };
 
 #endif
