aada5c0f2cdbbe8530b9bc2856f30e09d0796ce0
gabrielbosio <gabrielbosio95@gmail.com>
2019-06-13 20:09:17 -0300

    Refactoriza gestor de conexiones

--- src/GestorDeConexiones.cpp
+++ src/GestorDeConexiones.cpp
@@ -3,34 +3,94 @@
 #include <WiFi.h>
 #include "GestorDeConexiones.hpp"
 
-GestorDeConexiones::GestorDeConexiones() {}
+GestorDeConexiones::GestorDeConexiones(vector<Credencial*> credenciales) {
+    this->credenciales = credenciales;
+}
 
-void GestorDeConexiones::conectar(vector<Credencial*> credenciales) {
-    int cantidadDeAPs = WiFi.scanNetworks();
-    vector<string> ssidAPs;
+void GestorDeConexiones::conectar() {
+    buscarRedesYConectar();
+    Serial.print("Cantidad de credenciales:");
+    Serial.println(credenciales.size());
+}
 
-    for (int i = 0; i < cantidadDeAPs; i++) {
-        string ssidAP = WiFi.SSID(i).c_str();
-        ssidAPs.push_back(ssidAP);
-    }
+void GestorDeConexiones::buscarRedesYConectar() {
+    vector<string> redesDisponibles = obtenerRedesDisponibles();
 
-    for (int i = 0; i < credenciales.size(); i++) {
+    intentarConectarARedesDisponibles(redesDisponibles);
+}
+
+void GestorDeConexiones::intentarConectarARedesDisponibles(vector<string> redesDisponibles){
+    int i = 0;
+    int cantidadDeRedesDisponibles = redesDisponibles.size();
+
+    while (i < credenciales.size() && !estaConectado()) {
         Credencial* credencial = credenciales[i];
-        string ssid = credencial->ssid();
+        string redGuardada = credencial->ssid();
         string password = credencial->password();
+        Serial.print("Red guardada de credencial:");
+        Serial.println(redGuardada.c_str());
+        Serial.print("Password de credencial:");
+        Serial.println(password.c_str());
+        int j = 0;
 
-        for (int j = 0; j < cantidadDeAPs; j++) {
-            string ssidAP = ssidAPs[i];
+        while (j < cantidadDeRedesDisponibles && !estaConectado()) {
+            string redSeleccionada = redesDisponibles[j];
 
-            if (ssid.compare(ssidAP) == 0) {
-                WiFi.begin(ssid.c_str(), password.c_str());
+            if (redGuardada == redSeleccionada) {
+                int cantidadDeIntentos = 0;
+                WiFi.begin(redGuardada.c_str(), password.c_str());
 
-                if (WiFi.status() == WL_CONNECTED) {
+                while (!estaConectado() &&
+                    cantidadDeIntentos++ <= CANTIDAD_DE_INTENTOS_MAXIMA) {
+
+                    Serial.print("Conectando a ");
+                    Serial.println(redGuardada.c_str());
+                    delay(1000);
+                }
+
+                if (estaConectado()) {
                     Serial.print("Conectado a ");
-                    Serial.println(ssid.c_str());
-                    break;
+                    Serial.println(redGuardada.c_str());
+                } else {
+                    Serial.println("No se pudo conectar.");
                 }
             }
+
+            j++;
         }
+
+        i++;
     }
+}
+
+int GestorDeConexiones::obtenerCantidadDeRedesDisponibles() {
+    int cantidadDeRedesDisponibles = -1;
+    while(cantidadDeRedesDisponibles < 0){
+        cantidadDeRedesDisponibles = WiFi.scanNetworks();
+    }
+
+    return cantidadDeRedesDisponibles;
+}
+
+vector<string> GestorDeConexiones::obtenerRedesDisponibles() {
+    int cantidadDeRedesDisponibles = obtenerCantidadDeRedesDisponibles();
+
+    Serial.println("Buscando Access Points...");
+    Serial.print("Cantidad de APs:");
+    Serial.println(cantidadDeRedesDisponibles);
+    vector<string> redesDisponibles;
+
+    for (int i = 0; i < cantidadDeRedesDisponibles; i++) {
+        string ssidAP = WiFi.SSID(i).c_str();
+        redesDisponibles.push_back(ssidAP);
+        //Serial.print("SSID:");
+        //Serial.println(ssidAP.c_str());
+    }
+
+    return redesDisponibles;
+}
+
+bool GestorDeConexiones::estaConectado() {
+    Serial.println(WiFi.status());
+    return WiFi.status() == WL_CONNECTED;
 }
--- src/GestorDeConexiones.hpp
+++ src/GestorDeConexiones.hpp
@@ -7,8 +7,16 @@ using namespace std;
 
 class GestorDeConexiones {
     public:
-        GestorDeConexiones();
-        void conectar(vector<Credencial*> credenciales);
+        GestorDeConexiones(vector<Credencial*> credenciales);
+        void conectar();
+        bool estaConectado();
+    private:
+        const int CANTIDAD_DE_INTENTOS_MAXIMA = 10;
+        vector<Credencial*> credenciales;
+        void buscarRedesYConectar();
+        int obtenerCantidadDeRedesDisponibles();
+        vector<string> obtenerRedesDisponibles();
+        void intentarConectarARedesDisponibles(vector<string> redesDisponibles);
 };
 
 #endif
