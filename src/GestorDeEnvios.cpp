67f09b2cd1f47677c11fc0a9e96f542943d5c4e6
Guido Rombolá <guidorombola@gmail.com>
2019-06-27 13:12:52 -0300

    Remueve logica de conexion

--- src/GestorDeEnvios.cpp
+++ src/GestorDeEnvios.cpp
@@ -31,26 +31,3 @@ bool GestorDeEnvios::enviar(String contenidoArchivo){
 
     return pudoEnviar;
 }
-
-bool GestorDeEnvios::estaConectado(){
-    return WiFi.status() == WL_CONNECTED;
-}
-
-bool GestorDeEnvios::conectar(){
-    WiFi.begin("Fibertel WiFi190 2.4GHz", "telecomunicaciones96");
-
-    bool estaConectado = WiFi.status() == WL_CONNECTED;
-    int numeroDeIntentos = 0;
-
-    while (!estaConectado && numeroDeIntentos < CANTIDAD_DE_INTENTOS_MAXIMA) {
-        delay(1000);
-        Serial.println("Conectando");
-
-        estaConectado = WiFi.status() == WL_CONNECTED;
-        numeroDeIntentos++;
-    }
-
-    Serial.println("Conectado");
-
-    return estaConectado;
-}
